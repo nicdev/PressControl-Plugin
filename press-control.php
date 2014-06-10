26e1a591cdf09c1b1d9934a9adec273ed2cea072
Nic Rosental <nic@epiclabs.com>
2014-06-09 21:58:09 -0400

    stubbed out plugin options menu, completed header

--- press-control.php
+++ press-control.php
@@ -1,16 +1,33 @@
 <?php 
 
 /**
-* Plugin Name: Press Control
-* Description: Allows you to connect to PressControl's dashboard for better multisite management.
-* Version: 0.0.1
-* Author: Nic Rosental/Epic Labs nic@epiclabs.com
-*/
+ * Plugin Name: Press Control
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: Allows you to connect to PressControl's dashboard for better multisite management.
+ * Version: 0.1
+ * Author: Nic Rosental, Epic Labs 
+ * Author URI: http://epiclabs.com
+ * License: GPL2
+ * Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : PLUGIN AUTHOR EMAIL)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, version 2, as 
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
-register_activation_hook(__FILE__, pressControl('firstRun'));
-add_action('init', array('pressControl', 'adminPanel'));
+register_activation_hook(__FILE__, array('pressControl', 'firstRun'));
+add_action('admin_menu', array('pressControl', 'adminPanel'));
 
-class pressControl(){
+class pressControl {
 
 	/**
 	 * Sets up the cron job to start submitting info to pressControl
@@ -25,14 +42,13 @@ class pressControl(){
 
 	public function adminPanel(){
 
-		//hook up admin panel
+		add_management_page('Press Control', 'Press Control', 'activate_plugins', 'press-control-options', array('pressControl', 'adminPanelRender'));
 
 	}
 
 	public function adminPanelRender(){
-
-		//provide options for how often it submits info
-		//allow entering token for PressControl auth
+		
+		echo file_get_contents(dirname(__FILE__) . '/views/options-page.php');
 	
 	}
 }
---
+++ views/options-page.php
@@ -0,0 +1,3 @@
+<h2>Press Control</h2>
+
+Some ish goes here.
